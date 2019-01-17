af301d405b59d8088695c927181912e0a948d098
Luisfrb56 <luisfrb56@gmail.com>
2019-01-17 09:41:03 +0100

    Ejercicios de la segunda evaluacion, los primeros de Cliente servidor, y ejemplos Stream y Datagram

--- ProcesoEmisorDG/src/procesoemisordg/ProcesoEmisorDG.java
+++ ProcesoEmisorDG/src/procesoemisordg/ProcesoEmisorDG.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class ProcesoEmisorDG {
 
@@ -11,7 +12,9 @@ public class ProcesoEmisorDG {
         try{
             System.out.println("Creando socket datagram");
 
+            
             DatagramSocket datagramSocket=new DatagramSocket();
+
 
             System.out.println("enviando mensaje");
 
@@ -23,13 +26,24 @@ public class ProcesoEmisorDG {
 
             System.out.println("Mensaje enviado");
 
+            System.out.println("Recibiendo mensaje");
+            
+            InetSocketAddress addr2=new InetSocketAddress("localhost",5556);
+            datagramSocket=new DatagramSocket(addr2);
+            byte[] mensaje2=new byte[25];
+            DatagramPacket datagrama1=new DatagramPacket(mensaje2,25);
+            datagramSocket.receive(datagrama1);
+            System.out.println("Mensaje recibido: "+new String(mensaje2));
+            
             System.out.println("Cerrando el socket datagrama");
 
             datagramSocket.close();
 
             System.out.println("Terminado");
             }catch(IOException e){
+                
                 e.printStackTrace();
+                
             }
     }
 }
